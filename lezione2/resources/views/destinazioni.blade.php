8454ba0db8279e0565547a8edbd273caa9dce8ee
Gianclaudio <gianclaudiotarantini@gmail.com>
2023-05-10 18:29:28 +0200

    esercizio

--- lezione2/resources/views/destinazioni.blade.php
+++ lezione2/resources/views/destinazioni.blade.php
@@ -33,18 +33,7 @@
   </nav>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
   </script>
-  {{-- <?php
-    foreach($destinazioni as $mete){
-
-    }
-
-    ?> --}}
-
-    {{-- if($destinazioni){
-                
-    } else{
-        
-    } --}}
+ 
     <ul>
         @foreach($destinazioni as $mete)
         @if($mete != 'Carlo')
--- lezione2/resources/views/homepage.blade.php
+++ lezione2/resources/views/homepage.blade.php
@@ -21,7 +21,7 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="azienda">Features</a>
+          <a class="nav-link" href="#">Features</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Pricing</a>
@@ -30,7 +30,7 @@
     </div>
   </div>
 </nav>
-    <h1>Homepage</h1>
+    <h1>HOME PAGE</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
